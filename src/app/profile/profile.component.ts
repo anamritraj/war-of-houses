5a23d7d0e547598e4d5eb306b323332bd424be0e
anamritraj <anandamritraj904@gmail.com>
2017-02-23 23:47:48 +0530

    Add more components into profile component.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../shared/user.model";
+import {GameUserService} from "../services/game-user.service";
+import {TokenManager} from "../services/token-manager.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [GameUserService, TokenManager]
 })
 export class ProfileComponent implements OnInit {
+  user = new User();
 
-  constructor() { }
+  constructor(
+    private _gameUserService: GameUserService,
+    private _router: Router
+  ){}
 
   ngOnInit() {
+    this._gameUserService.getGameUser().subscribe((res) =>{
+      console.log(res);
+      this.user = res;
+    },(error) =>{
+      this._router.navigate(['/login']);
+    });
   }
 
 }
