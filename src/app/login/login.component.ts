ea71641297c5a3bde810d8422974f62f96222332
anamritraj <anandamritraj904@gmail.com>
2017-02-23 19:12:36 +0530

    Fix login issues.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FacebookService, FacebookAuthResponse, FacebookLoginResponse, FacebookLoginStatus} from '../services/facebook.service'
+import { FacebookService, FacebookAuthResponse } from '../services/facebook.service'
 import { TokenManager} from '../services/token-manager.service';
 import { UserService } from '../services/user.service';
 import { GameUserService } from '../services/game-user.service';
-import { User } from '../shared/user.model';
+import { Router } from '@angular/router';
 
+import { User } from '../shared/user.model';
 
 @Component({
   selector: 'app-login',
@@ -13,48 +14,71 @@ import { User } from '../shared/user.model';
   providers: [FacebookService, TokenManager, UserService, GameUserService]
 })
 export class LoginComponent implements OnInit {
-
-	userAccessToken: string;
-
 	showLoginForm: boolean;
 	loginError: string;
 	showRegisterForm: boolean;
-
-	user = new User();
-  
+	registerErrors: Object;
+	house_name: string;
   constructor(
-  	private _facebookService:FacebookService, 
-  	private _tokenManager: TokenManager, 
+  	private _facebookService:FacebookService,
+  	private _tokenManager: TokenManager,
   	private _userService: UserService,
   	private _gameUserService: GameUserService,
-  	) { }
+    private _route: Router
+  	)
+  {
+  	this.registerErrors = {};
+  }
 
   ngOnInit() {
-    // Check if the user has a valid access token in his browser.
-		// Get Access token from Local Storgae.
-		this.userAccessToken = this._tokenManager.retrieve();
+    //Get the access token from the browser and check if he is a valid user.
 
-		this._gameUserService.getGameUser().subscribe((res) =>{
-			console.log(res);
-			if(!res.ec_id){
-				// User is not registered for the game
-				this.show_RegisterForm();
-			}else{
-				this.user = res;
-				console.log(this.user);
-			}
-
+    this._gameUserService.getGameUser().subscribe((res) =>{
+      console.log(res);
+      if(Object.keys(res).length == 0){
+        //User is not registered for the game
+        this.show_RegisterForm();
+        console.log("Register form shown");
+      }else{
+        console.log("user already registered!");
+        //user is already registered for the game
+        this._route.navigate(['/profile']);
+      }
 		}, (error)=>{
-			this.doFacebookLogin().then((r)=>{
-				this._gameUserService.getGameUser().subscribe((result) => {
-					console.log(result);
-				},(error) => {
-					console.log("Something very wrong has happened. Call inform guy: +91 8463826679. He knows what to do.")
-				})		
-			})
+      console.log(error);
+      // Access token is invalid. Or the user is not registered! Lets try to get a new Access token.
+      /*
+        Perform facebook login
+        Get fb_access_token
+        Send it over to the server.
+        Validate the token
+        Get New token or Ask user to register on Eclectika Main website.
+        Navigate him to his profile.
+			*/
+			this.doFacebookLogin().then(()=>{
+          //the user is now logged in!
+          //Lets see i he is registered for the game!
+			    this._gameUserService.getGameUser().subscribe((res) => {
+            console.log(res);
+            if(Object.keys(res).length == 0){
+              //User is not registered for the game
+              this.show_RegisterForm();
+              console.log("Register form shown");
+            }else{
+              //user is already registered for the game
+              console.log("user is already regitered for the game!");
+              this._route.navigate(['/profile']);
+            }
+          },(error) => {
+			      console.log(error);
+            console.log("Something very wrong has happened. Inform this guy: +91 8463826679. He knows what to do.")
+          });
+			}, error => {
+			  console.log(error);
+      })
 		})
   }
-  
+
 	authenticate(fb_token: string): Promise<any>{
     return new Promise<any>((resolve, reject) =>{
     	this._userService.authenticate(fb_token).subscribe((res) => {
@@ -64,7 +88,7 @@ export class LoginComponent implements OnInit {
     			reject(null);
     		}
     	}, (error) =>{
-    		var err = JSON.parse(error._body);
+        let err = JSON.parse(error._body);
     		reject(err);
     	});
     });
@@ -75,7 +99,7 @@ export class LoginComponent implements OnInit {
    */
   doFacebookLogin(): Promise<any>{
     return new Promise<any>((resolve, reject) =>{
-	  	this._facebookService.init();    
+	  	this._facebookService.init();
 	  	this._facebookService.getLoginStatus().then((resp) =>{
 	  		console.log(resp);
 	  		if (resp.status == 'connected') {
@@ -83,16 +107,18 @@ export class LoginComponent implements OnInit {
 		      console.log('User is already connected with facebook. Now lets see if he is in the database!');
 		      // Get a new token and save it.
 		      this.getNewJWTToken(resp.authResponse).then((res)=>{
-		      	if(res) resolve(true);
+            resolve(true);
 		      }, (err)=>{
 		      	reject(err);
 		      });
 		    }else if(resp.status == 'not_authorized') {
 		    	console.log("not_authorized");
 	  			this.show_LoginForm("invalid_credentials");
+	  			resolve(false);
 		    }else{
-		      console.log("Unkonown Status");
+		      console.log("Unknown Status");
 	  			this.show_LoginForm("invalid_credentials");
+          resolve(false);
 		    }
 	  	});
 	  })
@@ -104,13 +130,25 @@ export class LoginComponent implements OnInit {
   	console.log("Register form shown");
   }
 
+  registerClick(){
+  	this.registerErrors = {};
+  	this._gameUserService.register(this.house_name).subscribe(res => {
+  		// Registration complete!
+  		// Now show him his dashboard
+      this._route.navigate(['/profile']);
+  	}, err => {
+  		// there was an error in the form
+  		this.registerErrors = JSON.parse(err._body);
+  	})
+  }
+
   show_LoginForm(msg:string){
   	if(msg == "invalid_credentials"){
-  		this.loginError = "Looks like you are not Registered for Eclectika 2017. Click the button below to register for Eclectika 2017.";	
+  		this.loginError = "Looks like you are not Registered for Eclectika 2017. Click the button below to register for Eclectika 2017.";
   	}else if(msg == "access_token_invalid"){
-  		this.loginError = "Looks like your session expired. Please reload the page.";	
+  		this.loginError = "Looks like your session expired. Please reload the page.";
   	}else{
-  		this.loginError = "Unknown Error!! :O Please drop a messgae to this guy: +91 84638 26679! Trust us he knows what to do.";	
+  		this.loginError = "Unknown Error!! :O Please drop a messgae to this guy: +91 84638 26679! Trust us he knows what to do.";
   	}
   	this.showLoginForm = true;
   	this.showRegisterForm = false;
@@ -124,21 +162,17 @@ export class LoginComponent implements OnInit {
 	  		console.log("JWT Token saved.");
 	  		resolve(true);
 	    },(error) => {
+	  	  console.log(error);
 	  		if(error.error == "invalid_credentials") {
-	  			console.log('Token ok, user is not in database.');
+	  			console.log('Facebook Token ok, user is not in database. We need to get this user registered First!');
 	  			resolve(true);
 	  		}else if(error.error == "access_token_invalid"){
-	  			console.log('Token is invalid');
+	  			console.log('Facebook Token is invalid. Perform login again!');
 	  			resolve(true);
 	  		}
 	  		this.show_LoginForm(error.error);
 	    });
-	    // reject("Unknown Error Occured in getNewJWTToken() reject");
 	  })
   };
 
-  alreadyLoggedIn(): Promise<any>{
-  	  return new Promise<any>((resolve, reject) =>{
-	  });
-  }
 }
