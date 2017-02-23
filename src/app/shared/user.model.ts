d56c5f4915fdfcf1298765097e068cde0e83dd3e
anamritraj <anandamritraj904@gmail.com>
2017-02-23 23:43:46 +0530

    Add more fields to user model.

--- src/app/shared/user.model.ts
+++ src/app/shared/user.model.ts
@@ -1,13 +1,15 @@
 export class User{
 	ec_id:string;
-	name: string;
+  name: string;
+  college: string;
+  semester: string;
 	house_name: string;
 	food:number;
 	gold:number;
 	wood:number;
 	turns:number;
 	army:number;
-	artillary:number;
+	artillery:number;
 	wall:number;
 	attack:number;
 	defence:number;
@@ -16,18 +18,19 @@ export class User{
 	can_be_attacked:number;
 	last_turn_refreshed:number;
 
-
 	constructor(){
 		this.ec_id = null;
 		this.house_name = null;
 		this.name = null;
+		this.college = null;
+		this.semester = null;
 		this.fb_id = null;
 		this.food = null;
 		this.gold = null;
 		this.wood = null;
 		this.turns = null;
 		this.army = null;
-		this.artillary = null;
+		this.artillery = null;
 		this.wall = null;
 		this.attack = null;
 		this.defence = null;
@@ -35,8 +38,4 @@ export class User{
 		this.can_be_attacked = null;
 		this.last_turn_refreshed = null;
 	}
-
-	setuser(user : User){
-		this.user = user;
-	}
-};
+}
