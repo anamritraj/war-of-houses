8b8d0ac697f8bc5ba02881f142ec7a3b932c7eac
anamritraj <anandamritraj904@gmail.com>
2017-02-24 17:28:14 +0530

    Update user model.

--- src/app/shared/user.model.ts
+++ src/app/shared/user.model.ts
@@ -3,39 +3,37 @@ export class User{
   name: string;
   college: string;
   semester: string;
-	house_name: string;
-	food:number;
-	gold:number;
-	wood:number;
-	turns:number;
-	army:number;
-	artillery:number;
-	wall:number;
-	attack:number;
-	defence:number;
-	fb_id: string;
-	fb_access_token: string;
-	can_be_attacked:number;
-	last_turn_refreshed:number;
 
-	constructor(){
-		this.ec_id = null;
-		this.house_name = null;
-		this.name = null;
-		this.college = null;
-		this.semester = null;
-		this.fb_id = null;
-		this.food = null;
-		this.gold = null;
-		this.wood = null;
-		this.turns = null;
-		this.army = null;
-		this.artillery = null;
-		this.wall = null;
-		this.attack = null;
-		this.defence = null;
-		this.fb_access_token = null;
-		this.can_be_attacked = null;
-		this.last_turn_refreshed = null;
-	}
+  house_name: string;
+
+	can_be_attacked:number;
+
+	food:number;
+  gold:number;
+	wood:number;
+
+  food_x: number;
+  gold_x: number;
+  wood_x: number;
+
+  last_food: string;
+  last_gold: string;
+  last_wood: string;
+
+  turns:number;
+  last_turn_refreshed:number;
+
+  workers:number;
+  last_worker:string;
+
+  army:number;
+  artillery:number;
+  wall:number;
+  dragons:number;
+
+  attack:number;
+  defence:number;
+  fb_id: string;
+  fb_access_token: string;
+
 }
