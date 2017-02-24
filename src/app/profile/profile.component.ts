6d601d58cf4b0151679bb7afb728d6d7b03cf03f
anamritraj <anandamritraj904@gmail.com>
2017-02-24 17:27:27 +0530

    Add Notifications in profile componenent.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,20 +11,38 @@ import {Router} from "@angular/router";
   providers: [GameUserService, TokenManager]
 })
 export class ProfileComponent implements OnInit {
-  user = new User();
 
+  user = new User();
+  notificationTitle: string;
+  notificaitonBody: string;
+  notificationType: string;
+  viewNotification: boolean;
   constructor(
     private _gameUserService: GameUserService,
-    private _router: Router
+    private _router: Router,
   ){}
 
   ngOnInit() {
+
     this._gameUserService.getGameUser().subscribe((res) =>{
       console.log(res);
-      this.user = res;
+      if(res.house_name){
+        console.log(res);
+        this.user = res;
+      }else{
+        this._router.navigate(['/login']);
+      }
     },(error) =>{
+      console.log(error);
       this._router.navigate(['/login']);
     });
   }
 
+  showNotification(noti){
+    console.log(noti);
+    this.notificationTitle = noti.message;
+    this.notificaitonBody = noti.title;
+    this.notificationType = noti.type;
+    this.viewNotification =  true;
+  }
 }
