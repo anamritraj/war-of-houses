de2bac85dbf4095845bec5487c32e5daafa8ee4b
anamritraj <anandamritraj904@gmail.com>
2017-02-25 12:46:07 +0530

    Add life to attack widget! :libra:

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {GameUserService} from "../services/game-user.service";
+import {User} from "../shared/user.model";
 
 @Component({
   selector: 'app-leaderboard',
@@ -9,6 +10,7 @@ import {GameUserService} from "../services/game-user.service";
 export class LeaderboardComponent implements OnInit {
   page: number;
   leaderboardObjects = [];
+  @Input() currentUser: User;
   constructor(
     private _gameService: GameUserService
   ) {
@@ -24,4 +26,27 @@ export class LeaderboardComponent implements OnInit {
     })
   }
 
+  decreaseTurns(userObject){
+    if(userObject.turns && userObject.turns - 1 > 0){
+      userObject.turns -= 1;
+    }else{
+      userObject.turns = 1;
+    }
+  }
+
+  increaseTurns(userObject){
+    if(userObject.turns){
+      if(userObject.turns + 1 <= this.currentUser.turns)
+        userObject.turns += 1;
+      else
+        userObject.turns = this.currentUser.turns;
+    }else{
+      userObject.turns = 1;
+    }
+  }
+
+  attackUser(user){
+    console.log(user);
+  //  TODO Perform an attack!
+  }
 }
