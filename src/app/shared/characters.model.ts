a34fd3ccb660e424fcb0aaef936cf8db81b27a8b
anamritraj <anandamritraj904@gmail.com>
2017-02-26 02:49:35 +0530

    Add character details! <3

--- src/app/shared/characters.model.ts
+++ src/app/shared/characters.model.ts
@@ -2,10 +2,29 @@ export const CHARACTERS = {
   food: `<p>Food is important for all life forms. Food gives power. <hr> Claim Bonus/60 mins</p>`,
   wood: `<p>Wood is important for making weapons and houses. It is one of the basic construction materials. <hr> Claim Bonus/60 mins</p>`,
   gold: `<p>Gold is precious. Gold is used in shields and weapons and shows majesty. <hr> Claim Bonus/60 mins</p>`,
-  army: `<p>An Army consists of a group of 10 pikemen known as a <i>Regiment</i>. The are the ones with most control. They deal 10 damage and provide 10 defence per regiment`,
-  giant: `<p>A Giant is literally a big human! They are a little low on the intellectual side. They deal 100 damage and provide 50 defence.</i>`,
-  wall: `<p>A wall is made up of wood and provides rigid defence against enemy sources! Deals 0 damage and 100 defence.</p>`,
-  dragon: `<p>A Dragon is the fiercest of them all. Listens to no one. Master of his own will. However can be trained by some! Destroys everything in its way! Provides 500 attack and 100 defence.</p>`,
+  army: `<p>An Army consists of a group of 10 pikemen known as a <i>Regiment</i>. The are the ones with most control. They deal 10 damage and provide 10 defence per regiment
+          <br> 
+          <i class="twa twa-lg twa-poultry-leg"></i>: 50 <br>          
+          <i class="twa twa-lg twa-deciduous-tree"></i>: 30 <br>
+          <i class="twa twa-lg twa-moneybag"></i>: 20
+          </p>`,
+  giant: `<p>A Giant is literally a big human! They are a little low on the intellectual side. They deal 100 damage and provide 50 defence. 
+          <br> 
+          <i class="twa twa-lg twa-poultry-leg"></i>: 200 <br>          
+          <i class="twa twa-lg twa-deciduous-tree"></i>: 100 <br>
+          <i class="twa twa-lg twa-moneybag"></i>: 150 <br>`,
+  wall: `<p>A wall is made up of wood and provides rigid defence against enemy sources! Deals 0 damage and 100 defence.
+        <br> 
+          <i class="twa twa-lg twa-poultry-leg"></i>: 30 <br>          
+          <i class="twa twa-lg twa-deciduous-tree"></i>: 70<br>
+          <i class="twa twa-lg twa-moneybag"></i>: 10
+        </p>`,
+  dragon: `<p>A Dragon is the fiercest of them all. Listens to no one. Master of his own will. However can be trained by some! Destroys everything in its way! Provides 500 attack and 100 defence.
+          <br> 
+          <i class="twa twa-lg twa-poultry-leg"></i>: 1000 <br>          
+          <i class="twa twa-lg twa-deciduous-tree"></i>: 700 <br>
+          <i class="twa twa-lg twa-moneybag"></i>: 1200 
+          </p>`,
   worker: `<p>A worker acts as a multiplier! Literally! Assign one to your claims and it will multiply! Provides no damage or Defence! <hr> 1 Worker/3 hours</p>`,
   turns: `<p>Turns represent the number of times you attack another house. You have limited number of turns and you will loose turns when you attack another house, even if you loose. Use them wisely. <hr> Claim Bonus/60 mins</p>`
 };
